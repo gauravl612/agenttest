737948389f6d391522be75eab08a0f45dc0f4531
gauravl612 <43071449+gauravl612@users.noreply.github.com>
2021-07-09 14:40:23 +0530

    ONEDEVOPS-1234 Python file changes for docker testing

--- Test.py
+++ Test.py
@@ -5,3 +5,5 @@ Added to test server config data
 Testing for webhook Git 
 
 http://54.183.34.239:8981/PlatformInsightsWebHook/insightsDevOpsWebHook?webHookName=GIT_WEBHOOK_SCG
+    
+    Docker file testing 
