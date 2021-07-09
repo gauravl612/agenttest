7ee701b9c34773ef01e39bc2c7fbe6134d975fb1
gauravl612 <43071449+gauravl612@users.noreply.github.com>
2021-07-09 14:41:49 +0530

    commit messages formatting changes

--- Test.py
+++ Test.py
@@ -5,5 +5,4 @@ Added to test server config data
 Testing for webhook Git 
 
 http://54.183.34.239:8981/PlatformInsightsWebHook/insightsDevOpsWebHook?webHookName=GIT_WEBHOOK_SCG
-    
     Docker file testing 
